c7cd8ef22b33904bea062259a5e235989b4e22f0
Malick Arsalan Office <malickarsalan@hotmail.com>
2018-08-30 18:35:45 +0500

    Complete Part 5 WebMethod overloading in asp net web services

--- WebServiceDemo/CalculatorWebService.asmx.cs
+++ WebServiceDemo/CalculatorWebService.asmx.cs
@@ -10,39 +10,54 @@ namespace WebServiceDemo
     /// Summary description for CalculatorWebService
     /// </summary>
     [WebService(Namespace = "http://pragimtech.com/webservices")]
-    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [WebServiceBinding(ConformsTo = WsiProfiles.None)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
     public class CalculatorWebService : System.Web.Services.WebService
     {
+        // ASP.NET Web Services tutorial by kudvenkat 1-4
+        //[WebMethod(EnableSession=true, Description= "This methode adds 2 numbers", CacheDuration=20)]
+        //public int Add(int firstNumber, int secondNumber)
+        //{
+        //    List<string> calculations;
 
-        [WebMethod(EnableSession=true, Description= "This methode adds 2 numbers", CacheDuration=20)]
+        //    if (Session["CALCULATIONS"] == null)
+        //    {
+        //        calculations = new List<string>();
+
+        //    }
+        //    else
+        //    {
+        //        calculations = (List<string>)Session["CALCULATIONS"];
+        //    }
+
+        //    string strRecentCalculation = firstNumber.ToString() + " + " 
+        //        + secondNumber.ToString() + " = " 
+        //        + (firstNumber + secondNumber).ToString();
+
+        //    calculations.Add(strRecentCalculation);
+        //    Session["CALCULATIONS"] = calculations;
+
+        //    return firstNumber + secondNumber;
+        //}
+        // ASP.NET Web Services tutorial by kudvenkat 1-4
+
+        // ASP.NET Web Services tutorial by kudvenkat 5
+        [WebMethod(MessageName="Add2Numbers")]
         public int Add(int firstNumber, int secondNumber)
         {
-            List<string> calculations;
-
-            if (Session["CALCULATIONS"] == null)
-            {
-                calculations = new List<string>();
-
-            }
-            else
-            {
-                calculations = (List<string>)Session["CALCULATIONS"];
-            }
-
-            string strRecentCalculation = firstNumber.ToString() + " + " 
-                + secondNumber.ToString() + " = " 
-                + (firstNumber + secondNumber).ToString();
-
-            calculations.Add(strRecentCalculation);
-            Session["CALCULATIONS"] = calculations;
-
             return firstNumber + secondNumber;
         }
 
-        [WebMethod(EnableSession= true)]
+        [WebMethod(MessageName="Add3Numbers")]
+        public int Add(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return firstNumber + secondNumber + thirdNumber;
+        }
+        // ASP.NET Web Services tutorial by kudvenkat 5
+
+        [WebMethod(EnableSession= true, Description="This methods show all the recent results")]
         public List<string> GetCalculations()
         {
             if (Session["CALCULATIONS"] == null)
